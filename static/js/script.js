c6383be3295ca0961d8f061738f824af8083470b
Tahsinul Haque Dhrubo <71776452+thDhrubo@users.noreply.github.com>
2022-06-13 03:19:49 +0600

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -23,3 +23,73 @@ function generateCat(){
     div.appendChild(image);
 
 }
+// Challenge 3
+function rpsGame(choice){
+    // console.log(choice)
+    var user,com;
+    user=choice.id;
+    com=numberToChoice(randToRpsInt())
+    // console.log('Comp',com);
+    results=decideWinner(user,com);
+    // console.log(results)
+    message=finalMessage(results)
+    // console.log(message)
+    rpsFrontEnd(choice.id,com,message)
+}
+
+function randToRpsInt(){
+    return Math.floor(Math.random()*3);
+}
+
+function numberToChoice(number){
+    return ['rock','paper','scissors'][number];
+}
+
+function decideWinner(user,com){
+    var rpsDatabase={
+        'rock':{'scissors':1,'rock':0.5,'paper':0},
+        'paper':{'scissors':0,'rock':1,'paper':0.5},
+        'scissors':{'scissors':0.5,'rock':0,'paper':1},
+    }
+    var yourScore=rpsDatabase[user][com];
+    var comScore=rpsDatabase[com][user];
+    return [yourScore,comScore];
+}
+function finalMessage([yourScore,comScore]){
+    if(yourScore === 0){
+        return{'message':'You Lost!!','color':'red'};
+    }
+    else if(yourScore===0.5) {
+        return {'message': 'You Tied!!', 'color': 'yellow'};
+    }
+    else
+    {
+        return {'message': 'You Won!!', 'color': 'green'};
+
+    }
+}
+
+function rpsFrontEnd(humanImageChoice,botImageChoice,finalMessage){
+    var imagesDatabase={
+        'rock':document.getElementById('rock').src,
+        'paper':document.getElementById('paper').src,
+        'scissors':document.getElementById('scissors').src,
+    }
+    // remove all images
+    document.getElementById('rock').remove();
+    document.getElementById('paper').remove();
+    document.getElementById('scissors').remove();
+
+    var humanDiv=document.createElement('div');
+    var botDiv=document.createElement('div');
+    var messageDiv=document.createElement('div');
+
+    humanDiv.innerHTML="<img src='"+imagesDatabase[humanImageChoice]+"' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(37,50,233,1)'>";
+    messageDiv.innerHTML="<h1 style='color: "+finalMessage['color']+"; font-size: 60px; padding: 30px;'>"+finalMessage['message']+"</h1>"
+    botDiv.innerHTML="<img src='"+imagesDatabase[botImageChoice]+"' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(243,38,24,1)'>";
+
+    document.getElementById('flex-box-rpx-div').append(humanDiv);
+    document.getElementById('flex-box-rpx-div').append(messageDiv)
+    document.getElementById('flex-box-rpx-div').append(botDiv);
+
+}
